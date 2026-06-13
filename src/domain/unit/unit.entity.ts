8b7ef734369bd8aa5d0c48f4c7c5bc88875f8db8
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-13 17:54:29 -0300

    i add unit.entity.ts into unit folder into folder domain to project

---
+++ src/domain/unit/unit.entity.ts
@@ -0,0 +1,17 @@
+export interface UnitEntity {
+    cmp_uuid: string;
+    uni_uuid: string;
+    uni_code: string;
+    uni_category: 'Residencial' | 'Comercial' | 'Socio Pleno' | 'Socio Deportivo' | 'Espacio Comun' | 'Parcela';
+    uni_status: 'Activo' | 'Inactivo' | 'En_Mantenimiento';
+    uni_financialcoefficient: number;
+    uni_baseamountcustom: number;
+    uni_locationdetails: string;
+    uni_metadata?: Record<string, any>; // Campo dinámico para la flexibilidad genérica
+    uni_istransferable: boolean;
+    uni_createdat: Date;
+    uni_updatedat: Date
+}
+
+//Update
+export type UnitUpdateData = Pick<UnitEntity, 'uni_code' | 'uni_category' | 'uni_status' | 'uni_financialcoefficient' | 'uni_baseamountcustom' | 'uni_locationdetails' | 'uni_metadata' | 'uni_istransferable'>;
